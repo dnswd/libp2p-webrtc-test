452d17cf533eb37188782fc8eeea5212718dcca2
Dennis Al Baihaqi Walangadi <dennis.walangadi@gmail.com>
2023-02-18 22:50:38 +0700

    attempt isomorphic node

--- a.js
+++ a.js
@@ -1,40 +1,88 @@
 import { createLibp2p } from 'libp2p'
 import { webRTCDirect } from '@libp2p/webrtc-direct'
+import { bootstrap } from '@libp2p/bootstrap'
 import { noise } from '@chainsafe/libp2p-noise'
 import { mplex } from '@libp2p/mplex'
 import { createFromJSON } from '@libp2p/peer-id-factory'
+import { akeys } from "./keys.js"
+import { pipe } from 'it-pipe'
+import { toString as uint8ArrayToString } from 'uint8arrays/to-string'
+import { fromString as uint8ArrayFromString } from 'uint8arrays/from-string'
 import wrtc from 'wrtc'
 
-const hardcodedPeerId = await createFromJSON({
-    "id": "12D3KooWCuo3MdXfMgaqpLC5Houi1TRoFqgK9aoxok4NK5udMu8m",
-    "privKey": "CAESQAG6Ld7ev6nnD0FKPs033/j0eQpjWilhxnzJ2CCTqT0+LfcWoI2Vr+zdc1vwk7XAVdyoCa2nwUR3RJebPWsF1/I=",
-    "pubKey": "CAESIC33FqCNla/s3XNb8JO1wFXcqAmtp8FEd0SXmz1rBdfy"
-})
+const bootstrapers = [
+    '/ip4/104.131.131.82/tcp/4001/p2p/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ',
+    '/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN',
+    '/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
+    '/dnsaddr/bootstrap.libp2p.io/p2p/QmZa1sAxajnQjVM8WjWXoMbmPd7NsWhfKsPkErzpm9wGkp',
+    '/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa',
+    '/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt'
+]
 
-const node = await createLibp2p({
-    // libp2p nodes are started by default, pass false to override this
-    start: false, // don't immediately start
-    peerId: hardcodedPeerId,
-    addresses: {
-        listen: ['/ip4/127.0.0.1/tcp/9090/http/p2p-webrtc-direct']
-    },
+const a_key = await createFromJSON(akeys)
+const protocol = "/echo/1.0.0"
+const libp2p = await createLibp2p({
+    peerId: a_key,
     transports: [webRTCDirect({ wrtc })],
+    streamMuxers: [mplex()],
     connectionEncryption: [noise()],
-    streamMuxers: [mplex()]
+    // define multiaddress for the transport protocol to listen
+    addresses: {
+        listen: [
+            '/ip4/0.0.0.0/tcp/10333/http/p2p-webrtc-direct',
+            '/ip4/0.0.0.0/tcp/0/http/p2p-webrtc-direct',
+        ],
+    },
+    peerDiscovery: [
+        bootstrap({
+            interval: 60e3,
+            list: bootstrapers
+        })
+    ]
 })
 
-// start libp2p
-await node.start()
-console.log('libp2p has started')
-
-node.connectionManager.addEventListener('peer:connect', (evt) => {
-    console.info(`Connected to ${evt.detail.remotePeer.toString()}!`)
+// Listen for new connections to peers
+libp2p.connectionManager.addEventListener('peer:connect', (evt) => {
+    console.log(`Connected to ${evt.detail.remotePeer.toString()}`)
 })
 
-const listenAddrs = node.getMultiaddrs()
+// Listen for peers disconnecting
+libp2p.connectionManager.addEventListener('peer:disconnect', (evt) => {
+    console.log(`Disconnected from ${evt.detail.remotePeer.toString()}`)
+})
+
+console.log("Status: libp2p started!")
+console.log(`libp2p id is ${libp2p.peerId.toString()}`)
+
+// Listen for new peers
+libp2p.addEventListener('peer:discovery', (evt) => {
+    console.log(`Found peer ${evt.detail.id.toString()}`)
+
+    // dial them when we discover them
+    libp2p.dialProtocol(evt.detail.id, protocol).catch(err => {
+        console.log(`Could not dial ${evt.detail.id}`, err)
+    }).then((stream) => {
+        console.log(`nodeA dialed to nodeB on protocol: ${protocol}`)
+        pipe(
+            // Source data
+            [uint8ArrayFromString('hey')],
+            // Write to the stream, and pass its output to the next function
+            stream,
+            // Sink function
+            async function (source) {
+                // For each chunk of data
+                for await (const data of source) {
+                    // Output the data
+                    console.log('received echo:', uint8ArrayToString(data.subarray()))
+                }
+            }
+        )
+    })
+})
+
+// Handle incoming connections for the protocol by piping from the stream
+// back to itself (an echo)
+await libp2p.handle('/echo/1.0.0', ({ stream }) => pipe(stream.source, stream.sink))
+
 console.log('libp2p is listening on the following addresses: ')
-node.getMultiaddrs().forEach((ma) => console.log(ma.toString()))
-
-// stop libp2p
-// await node.stop()
-// console.log('libp2p has stopped')
+libp2p.getMultiaddrs().forEach((ma) => console.log(ma.toString()))
--- b.js
+++ b.js
@@ -1,52 +1,91 @@
 import { createLibp2p } from 'libp2p'
 import { webRTCDirect } from '@libp2p/webrtc-direct'
-import { mplex } from '@libp2p/mplex'
-import { noise } from '@chainsafe/libp2p-noise'
 import { bootstrap } from '@libp2p/bootstrap'
+import { noise } from '@chainsafe/libp2p-noise'
+import { mplex } from '@libp2p/mplex'
+import { createFromJSON } from '@libp2p/peer-id-factory'
+import { bkeys } from "./keys.js"
+import { pipe } from 'it-pipe'
+import { toString as uint8ArrayToString } from 'uint8arrays/to-string'
+import { fromString as uint8ArrayFromString } from 'uint8arrays/from-string'
 import wrtc from 'wrtc'
 
+const bootstrapers = [
+    '/ip4/104.131.131.82/tcp/4001/p2p/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ',
+    '/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN',
+    '/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
+    '/dnsaddr/bootstrap.libp2p.io/p2p/QmZa1sAxajnQjVM8WjWXoMbmPd7NsWhfKsPkErzpm9wGkp',
+    '/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa',
+    '/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt'
+]
 
-// use the same peer id as in `listener.js` to avoid copy-pasting of listener's peer id into `peerDiscovery`
-const hardcodedPeerId = '12D3KooWCuo3MdXfMgaqpLC5Houi1TRoFqgK9aoxok4NK5udMu8m'
+const b_key = await createFromJSON(bkeys)
+const protocol = "/echo/1.0.0"
 const libp2p = await createLibp2p({
+    peerId: b_key,
     transports: [webRTCDirect({ wrtc })],
     streamMuxers: [mplex()],
     connectionEncryption: [noise()],
+    // define multiaddress for the transport protocol to listen
+    addresses: {
+        listen: [
+            '/ip4/0.0.0.0/tcp/10333/http/p2p-webrtc-direct',
+            '/ip4/0.0.0.0/tcp/0/http/p2p-webrtc-direct',            
+        ]
+    },
     peerDiscovery: [
         bootstrap({
-            list: [`/ip4/127.0.0.1/tcp/9090/http/p2p-webrtc-direct/p2p/${hardcodedPeerId}`]
+            interval: 60e3,
+            list: bootstrapers
         })
     ]
 })
 
-// const output = document.getElementById('output')
-
-// output.textContent = ''
-
-function log(txt) {
-    console.log("Log: ", txt)
-    // output.textContent += `${txt.trim()}\n`
-}
-
-// Listen for new peers
-libp2p.addEventListener('peer:discovery', (evt) => {
-    log(`Found peer ${evt.detail.id.toString()}`)
-
-    // dial them when we discover them
-    libp2p.dial(evt.detail.id).catch(err => {
-        log(`Could not dial ${evt.detail.id}`, err)
-    })
-})
-
 // Listen for new connections to peers
 libp2p.connectionManager.addEventListener('peer:connect', (evt) => {
-    log(`Connected to ${evt.detail.remotePeer.toString()}`)
+    console.log(`Connected to ${evt.detail.remotePeer.toString()}`)
 })
 
 // Listen for peers disconnecting
 libp2p.connectionManager.addEventListener('peer:disconnect', (evt) => {
-    log(`Disconnected from ${evt.detail.remotePeer.toString()}`)
+    console.log(`Disconnected from ${evt.detail.remotePeer.toString()}`)
 })
 
 console.log("Status: libp2p started!")
-log(`libp2p id is ${libp2p.peerId.toString()}`)
+console.log(`libp2p id is ${libp2p.peerId.toString()}`)
+
+// Listen for new peers
+libp2p.addEventListener('peer:discovery', (evt) => {
+    console.log(`Found peer ${evt.detail.id.toString()}`)
+
+    // dial them when we discover them
+    console.log('Dialing to peer:', evt.detail.id.toString())
+    libp2p.dialProtocol(evt.detail.id, protocol).catch(err => {
+        console.log(`Could not dial ${evt.detail.id}`, err)
+    }).then((stream) => {
+        console.log(`nodeB dialed to nodeA on protocol: ${protocol}`)
+        pipe(
+            // Source data
+            [uint8ArrayFromString('hey')],
+            // Write to the stream, and pass its output to the next function
+            stream,
+            // Sink function
+            async function (source) {
+                // For each chunk of data
+                for await (const data of source) {
+                    // Output the data
+                    console.log('received echo:', uint8ArrayToString(data.subarray()))
+                }
+            }
+        )
+    })
+})
+
+// Handle incoming connections for the protocol by piping from the stream
+// back to itself (an echo)
+await libp2p.handle(protocol, ({ stream }) => pipe(stream.source, stream.sink))
+
+console.log('libp2p is listening on the following addresses: ')
+libp2p.getMultiaddrs().forEach((ma) => console.log(ma.toString()))
+
+
